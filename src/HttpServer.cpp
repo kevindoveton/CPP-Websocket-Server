3abbf1eb141cc4542bd0ea913e9ec07ead6f7797
Kevin Doveton <kevindoveton@me.com>
2018-03-13 14:47:48 +1030

    update gitignore

---
+++ src/HttpServer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Kevin Doveton on 9/3/18.
+//
+
+#include "HttpServer.h"
---
+++ src/HttpServer.h
@@ -0,0 +1,14 @@
+//
+// Created by Kevin Doveton on 9/3/18.
+//
+
+#ifndef CPP_WEBSOCKET_SERVER_HTTPSERVER_H
+#define CPP_WEBSOCKET_SERVER_HTTPSERVER_H
+
+
+class HttpServer {
+
+};
+
+
+#endif //CPP_WEBSOCKET_SERVER_HTTPSERVER_H
